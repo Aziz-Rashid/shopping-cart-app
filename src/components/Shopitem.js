a2aa3def086d35fd54a575e7bb97fc98d86e020d
aziz <axixraxhid@gmail.com>
2020-06-22 17:59:41 +0500

    update search bar

--- src/components/Shopitem.js
+++ src/components/Shopitem.js
@@ -1,12 +1,21 @@
-import React,{useContext} from 'react'
+import React,{useContext, useState} from 'react'
 import {GlobalContext} from '../context/context'
 import Shopitemslist from './Shopitemslist'
 import './shopitemslist.css'
 export default function Shopitem() {
+    const [search,setsearch] = useState('')
     const {shopingcart} = useContext(GlobalContext)
+    const filtershirt = shopingcart.filter(country =>{
+       return country.text.toLowerCase().includes(search.toLowerCase())
+    })
     return (
         <div className="j">
-            {shopingcart.map((item)=>(<Shopitemslist item={item.items} text={item.text} key={item.id} img={item.image} amount={item.amount} para={item.para} deff={item.deff} id={item.id}/> ))}
+            <div>
+            <input className="e" type="text" placeholder="Search Shirt" onChange={(e) => setsearch(e.target.value)}/>
+            </div>
+        <div >
+            {filtershirt.map((item)=>(<Shopitemslist item={item.items} text={item.text} key={item.id} img={item.image} amount={item.amount} para={item.para} deff={item.deff} id={item.id} /> ))}
+        </div>
         </div>
     )
 }
